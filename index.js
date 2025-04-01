89e345bbc2bdae0d3016b200c6f853841a3f28a4
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-04-01 19:48:07 +0600

    removed depricated thing

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use("/api/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
     app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
