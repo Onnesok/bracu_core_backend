62dc58e5518db62d75346c9c2602affbec5c7c58
Ratul Hasan <66786636+Onnesok@users.noreply.github.com>
2025-04-01 19:37:25 +0600

    solved case

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/User");
+const User = require("../models/User.js");
 
 // User Registration
 exports.register = async (req, res) => {
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,4 @@
-const User = require("../models/User");
+const User = require("../models/User.js");
 
 // Update User Profile
 exports.updateProfile = async (req, res) => {
